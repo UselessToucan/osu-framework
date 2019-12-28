82940b185cf977c5b3c131b6d8b1abafa6b95330
Roman Kapustin <TocoToucanMS@gmail.com>
2019-12-28 17:15:13 +0300

    Add OneWayBinding

---
+++ osu.Framework/Bindables/Bindings/OneWayBinding.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Bindables.Bindings
+{
+    public class OneWayBinding<T> : Binding<T>
+    {
+        public OneWayBinding(Bindable<T> source, Bindable<T> target)
+            : base(source, target)
+        {
+            if (Source.TryGetTarget(out var sourceObj))
+                sourceObj.ValueChanged += Source_ValueChanged;
+        }
+
+        private void Source_ValueChanged(ValueChangedEvent<T> source)
+        {
+            if (Target.TryGetTarget(out var target))
+                target.Value = source.NewValue;
+        }
+    }
+}
