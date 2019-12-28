e23fb6a1cd03321b602d8ab7de08fd200e00114e
Roman Kapustin <TocoToucanMS@gmail.com>
2019-12-28 17:04:51 +0300

    Add base class for all bindings

---
+++ osu.Framework/Bindables/Bindings/Binding.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Bindables.Bindings
+{
+    public abstract class Binding
+    {
+        public readonly WeakReference<IBindable> Source;
+        public readonly WeakReference<IBindable> Target;
+
+        protected Binding(IBindable source, IBindable target)
+        {
+            Source = new WeakReference<IBindable>(source);
+            Target = new WeakReference<IBindable>(target);
+        }
+    }
+}
