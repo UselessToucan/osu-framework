19087a9f217c94fba1e1de7bcef65f11111112bb
Roman Kapustin <TocoToucanMS@gmail.com>
2019-12-28 17:18:24 +0300

    Add TwoWayBinding

---
+++ osu.Framework/Bindables/Bindings/TwoWayBinding.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Bindables.Bindings
+{
+    public class TwoWayBinding<T> : OneWayBinding<T>
+    {
+        public TwoWayBinding(Bindable<T> source, Bindable<T> target)
+            : base(source, target)
+        {
+            if (Target.TryGetTarget(out var targetObj))
+                targetObj.ValueChanged += Target_ValueChanged;
+            ;
+        }
+
+        private void Target_ValueChanged(ValueChangedEvent<T> target)
+        {
+            if (Source.TryGetTarget(out var source))
+                source.Value = target.NewValue;
+        }
+    }
+}
