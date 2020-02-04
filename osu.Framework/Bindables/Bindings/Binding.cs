3964cb2315591b72ebaa31805a95907909c7a788
Roman Kapustin <TocoToucanMS@gmail.com>
2020-02-04 23:26:03 +0300

    Add Binding abstract class

---
+++ osu.Framework/Bindables/Bindings/Binding.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Bindables.Bindings
+{
+    public abstract class Binding<T>
+    {
+        protected readonly WeakReference<Bindable<T>> Source;
+        protected readonly WeakReference<Bindable<T>> Target;
+
+        protected Binding(WeakReference<Bindable<T>> source, WeakReference<Bindable<T>> target)
+        {
+            Source = source;
+            Target = target;
+        }
+
+        public abstract void PropagateValueChange(Bindable<T> source);
+    }
+}
