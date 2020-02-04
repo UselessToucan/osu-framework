df1615286454a13e76c5f8fe0b835186d48037a5
Roman Kapustin <TocoToucanMS@gmail.com>
2020-02-05 00:01:42 +0300

    Add BindingMode enum

---
+++ osu.Framework/Bindables/Bindings/BindingMode.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Bindables.Bindings
+{
+    public enum BindingMode
+    {
+        OneWay, TwoWay
+    }
+}
