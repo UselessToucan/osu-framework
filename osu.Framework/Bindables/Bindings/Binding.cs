81b6fda0befcf860abebc5b8d8ddade167403083
Roman Kapustin <TocoToucanMS@gmail.com>
2020-03-15 20:22:51 +0300

    Comment Binding<T>

--- osu.Framework/Bindables/Bindings/Binding.cs
+++ osu.Framework/Bindables/Bindings/Binding.cs
@@ -5,11 +5,27 @@ using System;
 
 namespace osu.Framework.Bindables.Bindings
 {
+    /// <summary>
+    /// Denotes a binding between two <see cref="Bindable{T}"/> objects
+    /// </summary>
+    /// <typeparam name="T">The type of our stored <see cref="Bindable{T}.Value"/></typeparam>
     public abstract class Binding<T>
     {
+        /// <summary>
+        /// A binding source
+        /// </summary>
         protected readonly WeakReference<Bindable<T>> Source;
+
+        /// <summary>
+        /// A binding target
+        /// </summary>
         protected readonly WeakReference<Bindable<T>> Target;
 
+        /// <summary>
+        ///  Creates a new <see cref="Binding{T}"/> instance.
+        /// </summary>
+        /// <param name="source">A binding source</param>
+        /// <param name="target">A binding target</param>
         protected Binding(Bindable<T> source, Bindable<T> target)
         {
             Source = new WeakReference<Bindable<T>>(source);
