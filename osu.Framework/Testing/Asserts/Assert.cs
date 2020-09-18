da57c9eaf7a4eb83d0de5cfc11df55d4841288f1
Roman Kapustin <TocoToucanMS@gmail.com>
2020-09-19 01:44:54 +0300

    temp

---
+++ osu.Framework/Testing/Asserts/Assert.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Testing.Asserts
+{
+    public abstract class Assert<T>
+    {
+        public readonly string Description;
+        public readonly T Actual;
+        public readonly T Expected;
+
+        protected Assert(string description, ref T actual, ref T expected)
+        {
+            Description = description;
+            Actual = actual;
+            Expected = expected;
+        }
+
+        public void Evaluate()
+        {
+            if (!EvaluateInternal())
+                throw new AssertionException($"{Description} assertion failed.{Environment.NewLine}Expected:{Expected}{Environment.NewLine}Actual: {Actual}");
+        }
+
+        protected abstract bool EvaluateInternal();
+    }
+}
---
+++ osu.Framework/Testing/Asserts/EqualTo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Testing.Asserts
+{
+    public class EqualTo<T> : Assert<T> where T : IEquatable<T>
+    {
+        public EqualTo(string description, ref T actual, ref T expected)
+            : base(description, ref actual, ref expected)
+        {
+        }
+
+        protected override bool EvaluateInternal()
+        {
+            return Actual.Equals(Expected);
+        }
+    }
+}
