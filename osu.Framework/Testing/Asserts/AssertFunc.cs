26ec6dc5f6b40ead23de96137366f66c710faa13
Roman Kapustin <TocoToucanMS@gmail.com>
2020-09-23 00:14:41 +0300

    AssertFunc

---
+++ osu.Framework/Testing/Asserts/AssertFunc.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Testing.Asserts
+{
+    public class AssertFunc : Assert
+    {
+        private readonly Func<bool> func;
+
+        public AssertFunc(string description, Func<bool> func)
+            : base(description)
+        {
+            this.func = func;
+        }
+
+        protected override bool EvaluateInternal() => func();
+    }
+}
