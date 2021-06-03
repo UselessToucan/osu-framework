fa9d54ea0e169c8e346e430546acbabfb7ccc826
Roman Kapustin <TocoToucanMS@gmail.com>
2021-06-03 23:55:31 +0300

    Add InputQueue class hierarchy

---
+++ osu.Framework/Input/InputQueues/InputQueue.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+
+namespace osu.Framework.Input.InputQueues
+{
+    /// <summary>
+    /// Holds grouped drawables for input event propagation.
+    /// </summary>
+    public abstract class InputQueue
+    {
+        /// <summary>
+        /// Holds drawables that handles regular input.
+        /// </summary>
+        public readonly List<Drawable> Regular = new List<Drawable>();
+
+        /// <summary>
+        /// Clears underlying input queues.
+        /// </summary>
+        public virtual void Clear()
+        {
+            Regular.Clear();
+        }
+
+        /// <summary>
+        /// Reverses underlying input queues.
+        /// </summary>
+        public virtual void Reverse()
+        {
+            Regular.Reverse();
+        }
+    }
+}
---
+++ osu.Framework/Input/InputQueues/NonPositionalInputQueue.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Input.Bindings;
+
+namespace osu.Framework.Input.InputQueues
+{
+    public class NonPositionalInputQueue : InputQueue
+    {
+        /// <summary>
+        /// Holds drawables that handles key binding input.
+        /// </summary>
+        public readonly List<KeyBindingContainer> KeyBingingContainers = new List<KeyBindingContainer>();
+
+        public override void Clear()
+        {
+            base.Clear();
+            KeyBingingContainers.Clear();
+        }
+
+        public override void Reverse()
+        {
+            base.Reverse();
+            KeyBingingContainers.Reverse();
+        }
+    }
+}
