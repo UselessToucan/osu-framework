49e880e72c6d1785b1e805158891cf1dec3be73e
Roman Kapustin <TocoToucanMS@gmail.com>
2021-06-27 20:28:19 +0300

    Check focused for null

--- osu.Framework/Input/ButtonEventManager.cs
+++ osu.Framework/Input/ButtonEventManager.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using JetBrains.Annotations;
 using osu.Framework.Graphics;
@@ -72,7 +73,7 @@ namespace osu.Framework.Input
             List<Drawable> inputQueue;
             var focusedDrawable = InputQueue.GetFocusedDrawable();
 
-            if (handledBy == focusedDrawable)
+            if (handledBy != null && handledBy == focusedDrawable)
                 inputQueue = new List<Drawable>(1) { focusedDrawable };
             else if (handledBy is KeyBindingContainer)
                 inputQueue = InputQueue.KeyBingingContainers.OfType<Drawable>().ToList();
@@ -158,6 +159,9 @@ namespace osu.Framework.Input
         /// <returns>The drawable which handled the event or null if none.</returns>
         protected Drawable PropagateButtonEvent(IEnumerable<Drawable> drawables, UIEvent e)
         {
+            if(drawables == null)
+                Debugger.Break();
+
             var handledBy = drawables.ToList().FirstOrDefault(target => target.TriggerEvent(e));
 
             return propagateButtonEventInternal(e, handledBy);
